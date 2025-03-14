e8f9364e54f3d78b5b62e4d152066402e43f1720
Stanislav Zhiganov <zhi13@ya.ru>
2025-03-14 14:13:35 +0400

    Добавить зависимости, готовые шаблоны html

---
+++ practicum/src/main/java/ru/yandex/practicum/PracticumApplication.java
@@ -0,0 +1,13 @@
+package ru.yandex.practicum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticumApplication.class, args);
+	}
+
+}

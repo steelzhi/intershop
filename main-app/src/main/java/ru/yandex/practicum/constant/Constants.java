b156ff143916f07985c4bf9a06abb323c9859611
Stanislav Zhiganov <zhi13@ya.ru>
2025-05-05 12:55:43 +0400

    Внести коррективы в файл README.md

--- main-app/src/main/java/ru/yandex/practicum/constant/Constants.java
+++ main-app/src/main/java/ru/yandex/practicum/constant/Constants.java
@@ -2,8 +2,8 @@ package ru.yandex.practicum.constant;
 
 public class Constants {
     public static final String SCHEME = "http";
-    public static final String HOST = "localhost"; // Для запуска приложения локально
-    //public static final String HOST = "payment-service"; // Для запуска приложения в Docker
+    //public static final String HOST = "localhost"; // Для запуска приложения локально
+    public static final String HOST = "payment-service"; // Для запуска приложения в Docker
 
     public static final String PORT = "9090";
     public static final String ROOT_PATH = "/payments";
